f4f50f653191fc6e527aefe1a4c0670c3e6e8c3b
Sterling7718 <nora.sorence@gmail.com>
2024-04-03 08:17:54 +0800

    return

--- app/page.tsx
+++ app/page.tsx
@@ -1,123 +1,61 @@
-// import AcmeLogo from '@/app/ui/acme-logo';
+import AcmeLogo from '@/app/ui/acme-logo';
 
-// import styles from '@/app/ui/home.module.css';
+import styles from '@/app/ui/home.module.css';
 
-// import { ArrowRightIcon } from '@heroicons/react/24/outline';
-// import Link from 'next/link';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
-// import { lusitana } from './ui/fonts';
+import { lusitana } from './ui/fonts';
 
-// import Image from 'next/image';
-
-import { CheckboxIcon } from './login/CheckboxIcon';
-import React from 'react';
 import Image from 'next/image';
 
 import classes from './Login.module.css';
 export default function Page() {
   return (
-    // // These are Tailwind classes:
-    // <main className="flex min-h-screen flex-col p-6">
-    //   <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-    //     <AcmeLogo />
-    //   </div>
-    //   <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-    //     <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-    //       <div className={styles.shape} />
-    //       <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
-    //       <p
-    //         className={`${lusitana.className}
-    //         text-xl text-gray-800 md:text-3xl md:leading-normal`}
-    //       >
-    //         <strong>Welcome to Acme.</strong> This is the example for the{' '}
-    //         <a href="https://nextjs.org/learn/" className="text-blue-500">
-    //           Next.js Learn Course
-    //         </a>
-    //         , brought to you by Vercel.
-    //       </p>
-    //       <Link
-    //         href="/login"
-    //         className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-    //       >
-    //         <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-    //       </Link>
-    //     </div>
-    //     <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-    //       {/* Add Hero Images Here */}
-    //       <Image
-    //         src="/hero-desktop.png"
-    //         width={1000}
-    //         height={760}
-    //         className="hidden md:block"
-    //         alt="Screenshots of the dashboard project showing desktop version"
-    //       />
-    //       <Image
-    //         src="/hero-mobile.png"
-    //         width={560}
-    //         height={620}
-    //         className="block md:hidden"
-    //         alt="Screenshots of the dashboard project showing mobile version"
-    //       />
-    //     </div>
-    //   </div>
-    // </main>
-
-    <div className={classes.root}>
-      <div className={classes.mainContainer}>
-        <div className={classes.studentClearance}>
-          <div className={classes.textBlock}>Student</div>
-          <div className={classes.textBlock2}>Clearance</div>
+    // These are Tailwind classes:
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <AcmeLogo />
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div className={styles.shape} />
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+          <p
+            className={`${lusitana.className}
+            text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <a href="https://nextjs.org/learn/" className="text-blue-500">
+              Next.js Learn Course
+            </a>
+            , brought to you by Vercel.
+          </p>
+          <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
         </div>
-        <div className={classes.frame1}>
-          <button className={classes.buttonMSOA}>
-            <div className={classes.image3}></div>
-            <div className={classes.office365Login}>Office 365 Login</div>
-          </button>
-          <div className={classes.or}>or</div>
-          <div className={classes.rectangle3}></div>
-          <div className={classes.lOGIN}>LOGIN</div>
-          <div className={classes.rememberMeCheckBox}>
-            <div className={classes.checkbox}>
-              <CheckboxIcon className={classes.icon} />
-            </div>
-            <div className={classes.rememberMe}>Remember me</div>
-          </div>
-          <div className={classes.input}>
-            <div className={classes.passwordInput}>
-              <div className={classes.password}>Password</div>
-              <div className={classes.textField}>
-                <div className={classes.enter6CharactersOrMore}>
-                  <div className={classes.textBlock3}>
-                    Enter 6 characters or more
-                  </div>
-                  <div className={classes.textBlock4}>
-                    <p></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={classes.emailInput}>
-              <div className={classes.email}>Email</div>
-              <div className={classes.textField2}>
-                <div className={classes.youSjdelmonteStiEduPh}>
-                  you@sjdelmonte.sti.edu.ph
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={classes.loginWithOffice365}>
-            <div className={classes.textBlock5}>Login</div>
-            <div className={classes.textBlock6}>
-              <p className={classes.labelWrapper}>
-                <span className={classes.label}>with </span>
-                <span className={classes.label2}>Office 365</span>
-              </p>
-            </div>
-          </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
-      <div className={classes.stiLogo}></div>
-      <div className={classes.credits}></div>
-    </div>
+    </main>
   );
 }
